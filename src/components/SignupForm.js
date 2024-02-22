727c312b0312367eb9cf81e1d547a061dc36aa86
A.chitrarasu <chitrarasumit@gmail.com>
2024-02-22 16:26:02 +0530

    updated

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,10 +1,10 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form, Button, Label, Input, FormGroup } from "reactstrap"
 import toast from 'react-hot-toast';
 import { registerDeliveryAgent } from '../service/API';
 import axios from 'axios';
-const SignupForm = () => {
 
+const SignupForm = () => {
   const [ipAddress, setIpAdress] = useState("");
 
   const [agentDetails, setAgentDetails] = useState({
@@ -18,63 +18,60 @@ const SignupForm = () => {
     city: "",
     lat: "",
     lon: ""
-
-  })
+  });
 
   const getVisitorsIp = async () => {
     try {
-      const response = await axios.get("https://api.ipify.org")
-      setIpAdress(response.data)
+      const response = await axios.get("https://api.ipify.org");
+      setIpAdress(response.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    getVisitorsIp()
-  }, [])
-
+    getVisitorsIp();
+  }, []);
 
   const getLocation = async () => {
-    const response = await axios.get(`http://ip-api.com/json/${ipAddress}`)
+    const response = await axios.get(`http://ip-api.com/json/${ipAddress}`);
 
     if (response.data.status === "success" && response.data.city && response.data.zip) {
-
       setAgentDetails({
         ...agentDetails,
         city: response.data.city,
         pincode: response.data.zip,
         lon: response.data.lon,
         lat: response.data.lat
-      })
+      });
     }
-  }
+  };
+
   useEffect(() => {
     if (ipAddress) {
-      getLocation()
+      getLocation();
     }
-  }, [ipAddress,getLocation])
-
-
+  }, [ipAddress, getLocation]);
 
   const validation = () => {
     if (agentDetails.password !== agentDetails.confirmPass) {
-      toast.error('Password and Confirm password sholud be same')
-      return false
+      toast.error('Password and Confirm password sholud be same');
+      return false;
     }
 
     if (agentDetails.password.length < 5) {
-      toast.error("password should be more more than four charactors")
-      return false
+      toast.error("password should be more more than four charactors");
+      return false;
     }
-    return true
-  }
+    return true;
+  };
+
   const submitted = async (e) => {
     try {
-      e.preventDefault()
-      const isValidate = validation()
+      e.preventDefault();
+      const isValidate = validation();
       if (isValidate) {
-        const response = await registerDeliveryAgent(agentDetails.name, agentDetails.mobileNUmber, agentDetails.email, agentDetails.password, agentDetails.address, agentDetails.pincode, agentDetails.city, agentDetails.lon, agentDetails.lat)
+        const response = await registerDeliveryAgent(agentDetails.name, agentDetails.mobileNUmber, agentDetails.email, agentDetails.password, agentDetails.address, agentDetails.pincode, agentDetails.city, agentDetails.lon, agentDetails.lat);
         if (response.data.result) {
           setAgentDetails({
             ...agentDetails,
@@ -88,21 +85,17 @@ const SignupForm = () => {
             city: "",
             lat: "",
             lon: ""
-          })
+          });
 
-          toast.success(response.data.message)
+          toast.success(response.data.message);
         } else {
-          toast.error(response.data.message)
+          toast.error(response.data.message);
         }
-
       }
-
     } catch (error) {
-
-      console.log(error)
+      console.log(error);
     }
-
-  }
+  };
 
   return (
     <div className='hole-form'>
@@ -113,7 +106,6 @@ const SignupForm = () => {
             <Form onSubmit={submitted}>
               <FormGroup>
                 <Input
-
                   id="exampleEmail"
                   name="name"
                   placeholder=" name"
@@ -134,7 +126,6 @@ const SignupForm = () => {
                   required
                   value={agentDetails.mobileNUmber}
                   onChange={(e) => setAgentDetails({ ...agentDetails, mobileNUmber: e.target.value })}
-
                 />
               </FormGroup>
               <FormGroup>
@@ -152,12 +143,10 @@ const SignupForm = () => {
                   required
                   value={agentDetails.email}
                   onChange={(e) => setAgentDetails({ ...agentDetails, email: e.target.value })}
-
                 />
               </FormGroup>
               {' '}
               <FormGroup>
-
                 <Input
                   id="examplePassword"
                   name="password"
@@ -166,11 +155,9 @@ const SignupForm = () => {
                   required
                   value={agentDetails.password}
                   onChange={(e) => setAgentDetails({ ...agentDetails, password: e.target.value })}
-
                 />
               </FormGroup>
               <FormGroup>
-
                 <Input
                   id="examplePassword1"
                   name="password1"
@@ -179,10 +166,8 @@ const SignupForm = () => {
                   required
                   value={agentDetails.confirmPass}
                   onChange={(e) => setAgentDetails({ ...agentDetails, confirmPass: e.target.value })}
-
                 />
               </FormGroup>
-
               <FormGroup>
                 <div className='d-flex gap-1'>
                   <Input
@@ -193,7 +178,6 @@ const SignupForm = () => {
                     required
                     value={agentDetails.address}
                     onChange={(e) => setAgentDetails({ ...agentDetails, address: e.target.value })}
-
                   />
                   <Input
                     id="pincode"
@@ -205,7 +189,6 @@ const SignupForm = () => {
                     placeholder="pincode"
                     value={agentDetails.pincode}
                     onChange={(e) => setAgentDetails({ ...agentDetails, pincode: e.target.value })}
-
                   />
                 </div>
               </FormGroup>
@@ -221,7 +204,6 @@ const SignupForm = () => {
                     required
                     value={agentDetails.lon}
                     onChange={(e) => setAgentDetails({ ...agentDetails, lon: e.target.value })}
-
                   />
                   <Input
                     id="latitude"
@@ -230,12 +212,10 @@ const SignupForm = () => {
                     required
                     pattern="[0-9]+(\.[0-9]+)?" value={agentDetails.lat}
                     onChange={(e) => setAgentDetails({ ...agentDetails, lat: e.target.value })}
-
                   />
                 </div>
               </FormGroup>
               <FormGroup >
-
                 <Input
                   id="city"
                   name="city"
@@ -244,10 +224,8 @@ const SignupForm = () => {
                   required
                   value={agentDetails.city}
                   onChange={(e) => setAgentDetails({ ...agentDetails, city: e.target.value })}
-
                 />
               </FormGroup>
-
               <Button type='submit' className='colors '>
                 SIGN UP
               </Button>
