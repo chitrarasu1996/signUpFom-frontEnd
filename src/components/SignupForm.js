216a3052af37e5414762da2a28f249eec18a1778
A.chitrarasu <chitrarasumit@gmail.com>
2024-02-22 16:18:12 +0530

    updated

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import { Form, Button, Label, Input, FormGroup } from "reactstrap"
 import toast from 'react-hot-toast';
 import { registerDeliveryAgent } from '../service/API';
